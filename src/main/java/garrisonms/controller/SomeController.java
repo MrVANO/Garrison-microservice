b8f5e946454ee8d8c16e2e4a45f5169fbcfedbda
Ivan Frolov <Ivan.Frolov@bsc-ideas.com>
2020-06-26 18:08:04 +0300

    Changes for test CI/CD

--- src/main/java/garrisonms/controller/SomeController.java
+++ src/main/java/garrisonms/controller/SomeController.java
@@ -11,6 +11,6 @@ public class SomeController {
     @ResponseBody
     @RequestMapping(value = "/cicdtest", method = RequestMethod.GET)
     public String ciCdTest() {
-        return "CI/CD test v 0.1 worked";
+        return "CI/CD test v 0.2 worked";
     }
 }
