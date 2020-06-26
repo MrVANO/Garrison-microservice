3b24f7b1de4a64531d9455733f7b3fd22e3568c6
Ivan Frolov <Ivan.Frolov@bsc-ideas.com>
2020-06-26 17:26:09 +0300

    Changes for test CI/CD

--- src/main/java/garrisonms/controller/SomeController.java
+++ src/main/java/garrisonms/controller/SomeController.java
@@ -2,12 +2,15 @@ package garrisonms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SomeController {
 
-    @RequestMapping("api/some")
-    public String someMethod() {
-        return "done";
+    @ResponseBody
+    @RequestMapping(value = "/cicdtest", method = RequestMethod.GET)
+    public String ciCdTest() {
+        return "CI/CD test v 0.1 worked";
     }
 }
